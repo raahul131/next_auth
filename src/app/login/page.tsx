283a3c093e5f4a0b48458bb7747957ee076fcadf
Rahul R Yadav <rryadav2025@gmail.com>
2023-07-21 09:32:22 +0530

    Initialization of the Repo

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="">
+      <h1 className="text-center text-white text-2xl">hello from the LogIn</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
